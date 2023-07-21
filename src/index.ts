e0aac2709472386c5fead3c674d3147a2352e137
Will Sutton <wsutton17@gmail.com>
2023-07-21 02:20:38 -0400

    launch frontend works, need to direct ws to remote

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import express from "express";
 import path from "path";
+import { setupProxy } from "./shared/proxy/basic";
 
 import mockRunPredict from "./data/mock-data/mock.run.predict.json";
 import mockInternalProgress from "./data/mock-data/mock.internal.progress.json";
 import mockInfo from "./data/mock-data/mock.info.json";
 
+const proxyPort = 8080;
+const proxyHost = "127.0.0.1";
+
 const app = express();
 
-console.log(mockInfo)
-
-// Serve static files
+// Serve static files of UI
 app.use(express.static(path.join(__dirname, "../front-ui/v1")));
 
+// Mock responses to XHR calls made from UI onLoad
 app.get("/info", (req, res) => {
     res.json(mockInfo)
     // this mock data might not have to be accurate or complete,
@@ -29,6 +32,7 @@ app.post("/internal/progress", (req, res) => {
     res.json(mockInternalProgress)
 })
 
+// Define our app's routes here
 app.get("/admin", (req, res) => {
     res.json({"data":["admin", "panel"]})
 })
@@ -38,3 +42,10 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 });
+
+const proxyServer = setupProxy()
+
+proxyServer.listen(proxyPort, proxyHost, () => {
+    console.log(`Proxy is running on host ${proxyHost} port ${proxyPort}`);
+});
+
--- src/shared/proxy/basic.ts
+++ src/shared/proxy/basic.ts
@@ -1,21 +1,32 @@
 import http from "http";
 import httpProxy from "http-proxy";
 
-const proxy = httpProxy.createProxyServer({
-  // target: TARGET_HOST,
-  ws: true,
-});
+export function setupProxy(
+  wsHost : string = "127.0.0.1",
+  wsPort : number = 7861,
+  httpHost : string = "127.0.0.1",
+  httpPort : number = 3000,
+) {
+  const proxy = httpProxy.createProxyServer({
+    ws: true,
+  });
 
-const server = http.createServer(() => {
-  // proxy.web(req, res, {target: `http://${wsHost}:${httpPort}`});
-});
+  const server = http.createServer((req, res) => {
+    proxy.web(req, res, {target: `http://${httpHost}:${httpPort}`});
+  });
 
-server.on("upgrade", (req, socket, head) => {
-  if (req.headers["lightning_r_hash"]) {
-    console.log("lightning_r_hash: ", req.headers["lightning_r_hash"]);
-    proxy.ws(req, socket, head);
-  } else {
-    console.log("...403, no r_hash");
-    // send back 403
-  }
-});
+  server.on("upgrade", (req, socket, head) => {
+    // return
+    proxy.ws(req, socket, head, {target: `ws://${wsHost}:${wsPort}`});
+  });
+
+  proxy.on("error", (err) => {
+    console.error("proxy ws err: ", err);
+  });
+
+  server.on("error", (err) => {
+    console.error("proxy server err: ", err);
+  });
+
+  return server;
+}
