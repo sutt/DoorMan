59eec5c4c5007da18561d23cec4be4435a949e8f
Will Sutton <wsutton17@gmail.com>
2023-07-21 01:44:38 -0400

    express app run with ts; yay!

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,36 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const mockRunPredict = require('./mock-data/mock.run.predict.json');
-const mockInternalProgress = require('./mock-data/mock.internal.progress.json');
-const mockInfo = require('./mock-data/mock.info.json');
+import express from "express";
+import path from "path";
 
-// console.log(mockInfo)
+import mockRunPredict from "./data/mock-data/mock.run.predict.json";
+import mockInternalProgress from "./data/mock-data/mock.internal.progress.json";
+import mockInfo from "./data/mock-data/mock.info.json";
+
+const app = express();
+
+console.log(mockInfo)
 
 // Serve static files
-app.use(express.static(path.join(__dirname, '../front-ui/v1')));
+app.use(express.static(path.join(__dirname, "../front-ui/v1")));
 
-app.get('/info', (req, res) => {
-    // res.json({'a':'ok'})
+app.get("/info", (req, res) => {
     res.json(mockInfo)
+    // this mock data might not have to be accurate or complete,
+    // as long as client frontend just parses the json successfully
+    // it will prevent other things from breaking
 })
 
-app.post('/run/predict', (req, res) => {
+app.post("/run/predict", (req, res) => {
     res.json(mockRunPredict)
     // can use this route for timing of when frontend is loaded
     // and switch ws proxy to the right target
 })
 
-app.post('/internal/progress', (req, res) => {
+app.post("/internal/progress", (req, res) => {
     res.json(mockInternalProgress)
 })
 
-app.get('/admin', (req, res) => {
-    res.json({'data':['admin', 'panel']})
+app.get("/admin", (req, res) => {
+    res.json({"data":["admin", "panel"]})
 })
 
 const port = process.env.PORT || 3000;
