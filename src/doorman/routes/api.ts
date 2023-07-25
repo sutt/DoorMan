402fcec96ab7aa1961f3e0a7404550cd910e4733
Will Sutton <wsutton17@gmail.com>
2023-07-24 20:12:19 -0400

    doorman admin ui has check_payment polling

--- src/doorman/routes/api.ts
+++ src/doorman/routes/api.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express';
 import QRCode from 'qrcode';
-import { callCreateInvoicePayment } from '../services/funding';
+import { callCreateInvoicePayment, callCheckInvoicePayment } from '../services/funding';
 import { PaymentResponse } from '../services/funding';
 
 const router = express.Router();
+
+// These respond to XHR calls from the admin webpage scripts
 
 router.get('/' , async (req: Request, res: Response) => {
     res.send('Hello World!');
@@ -13,7 +15,7 @@ router.post('/funding_request' , async (req: Request, res: Response) => {
     try {
         const {amount, workerAddr} = req.body;
         // parseInt(amount);
-        const response : PaymentResponse | null = await callCreateInvoicePayment(workerAddr, amount, null);
+        const response : PaymentResponse | undefined = await callCreateInvoicePayment(workerAddr, amount, null);
         if (response) {
             const qrDataURL = await QRCode.toDataURL(response.payment_request);
             res.json({ qrDataURL: qrDataURL, data: response });
@@ -25,6 +27,16 @@ router.post('/funding_request' , async (req: Request, res: Response) => {
         res.status(500).json({ error: error.message });
     }
     
+});
+
+router.post('/check_payment', async (req: Request, res: Response) => {
+    try {
+        const { workerAddr, checkingId } = req.body;
+        const isPaid: boolean = await callCheckInvoicePayment(workerAddr, checkingId);
+        res.json({ is_paid: isPaid });
+    } catch (error) {
+        res.status(500).json({ is_paid: false, error: error.message });
+    }
 });
 
 export default router;
